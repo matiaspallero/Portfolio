08295242fb52f463cbcc7b8541246e4f344cbf53
Matias Pallero <matuutepallero@gmail.com>
2025-06-20 01:34:25 -0300

    limpieza de error en projects

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,7 @@
 // src/components/Hero.jsx
-import { useNavigate } from "react-router-dom";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 export default function Hero() {
-  const navigate = useNavigate();
 
   return (
     <section className="bg-gradient-to-br from-gray-900 to-gray-800 py-20 px-6 text-white">
@@ -11,12 +9,12 @@ export default function Hero() {
         <h1 className="text-5xl md:text-6xl font-bold mb-8">
           Hola, soy <span className="text-cyan-400">Matias Pallero</span>
         </h1>
-        <button 
-          onClick={() => navigate("/projects")} 
-          className="bg-cyan-500 hover:bg-cyan-600 px-6 py-3 rounded-lg font-semibold flex items-center gap-2 mx-auto transition-all"
+        <a
+          href="#projects"
+          className="bg-cyan-500 hover:bg-cyan-600 w-52 px-6 py-3 rounded-lg font-semibold flex items-center gap-2 mx-auto transition-all"
         >
           Ver mis proyectos <ArrowRightIcon className="h-5 w-5" />
-        </button>
+        </a>
       </div>
     </section>
   );
